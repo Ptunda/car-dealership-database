7150019b573f51420aaa052c224b22c654832a26
Ptunda <ptunda@pgh.yearup.org>
2024-06-12 22:32:42 -0400

    attempted Question 5

---
+++ find_dealerships_that_have_sedan.sql
@@ -0,0 +1,8 @@
+
+-- Question 5
+
+SELECT i.dealership_id, i.vin, d.name, d.address, v.make, v.model, v.SOLD
+FROM inventory i
+INNER JOIN dealerships d ON i.dealership_id = d.dealership_id
+INNER JOIN vehicles v ON i.vin = v.vin
+WHERE v.type = 'sedan';
