cc412508411d0c17e5936c94ceba9cda9ed62089
Ptunda <ptunda@pgh.yearup.org>
2024-06-12 21:41:10 -0400

    Populated and updated tables

--- db_script.sql
+++ db_script.sql
@@ -1,4 +1,6 @@
 
+-- Creating dealershipDB and providing related tables
+
 DROP DATABASE IF EXISTS dealershipDB;
 CREATE DATABASE dealershipDB;
 
@@ -24,7 +26,7 @@ CREATE TABLE vehicles(
     type VARCHAR(20) NOT NULL,
     color VARCHAR(20) NOT NULL,
     odometer INT,
-    price DECIMAL(15, 2)
+    price DECIMAL(15, 2),
     CONSTRAINT price_check CHECK(price > 0.00)
 );
 ALTER TABLE vehicles
@@ -66,3 +68,101 @@ CREATE TABLE lease_contracts(
     lease_date_and_time DATETIME,
     CONSTRAINT fk_lcv_vin FOREIGN KEY(vin) REFERENCES vehicles(vin)
 );
+
+-- populating and updating tables
+
+-- pertaining to dealerships table
+INSERT INTO dealerships(name, address, phone)
+VALUES	('Car World', '123 Main St', '555-123-4567'),
+		('Monroeville Best Cars', '5154 William Penn St', '555-555-5555'),
+        ('Robinson New & Used Cars', 'Robinson Township', '555-123-5555'),
+        ('Vibes Car World', '4707 Baum Blvd', '555-555-4567')
+;
+
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM dealerships;
+
+ALTER TABLE dealerships
+AUTO_INCREMENT = 1234;
+
+INSERT INTO dealerships(name, address, phone)
+VALUES	('Car World', '123 Main St', '555-123-4567'),
+		('Monroeville Best Cars', '5154 William Penn St', '555-555-5555'),
+        ('Robinson New & Used Cars', 'Robinson Township', '555-123-5555'),
+        ('Vibes Car World', '4707 Baum Blvd', '555-555-4567')
+;
+SET SQL_SAFE_UPDATES = 1;
+
+-- pertaining to vehicles table
+ALTER TABLE vehicles
+ADD COLUMN new BOOLEAN DEFAULT 0;
+
+INSERT INTO vehicles (vin, year, make, model, type, color, odometer, price)
+VALUES	(10234, 2015, 'Toyota', 'Camry', 'sedan', 'silver', 12000, 15995.99),
+		(10567, 2018, 'Honda', 'Civic', 'sedan', 'black', 8000, 18995.99),
+        (10987, 2017, 'Ford', 'Escape', 'SUV', 'white', 25000, 20995.99),
+        (10999, 2024, 'Ford', 'Escape', 'SUV', 'blue', 15, 40995.99),
+        (11000, 2018, 'Chevrolet', 'Malibu', 'sedan', 'red', 12000, 16995.99),
+        (11111, 2019, 'Toyota', 'RAV4', 'SUV', 'blue', 10000, 27995.99)
+;
+
+ALTER TABLE vehicles
+RENAME COLUMN new TO status;
+
+UPDATE vehicles
+SET status = 1
+WHERE vin = 10999;
+
+ALTER TABLE vehicles
+ADD COLUMN electric BOOLEAN DEFAULT 0;
+
+ALTER TABLE vehicles
+MODIFY COLUMN SOLD BOOLEAN
+AFTER electric;
+
+UPDATE vehicles
+SET electric = 1
+WHERE vin = 10999;
+
+INSERT INTO vehicles(vin, year, make, model, type, color, odometer, price, status, electric)
+VALUES	(11133, 2024, 'Porsche', 'Taycan Turbo S', 'sedan', 'white', 0, 194499.99, 1, 1),
+		(11233, 2022, 'Tesla', 'Model 3', 'sedan', 'red', 8000, 37998.99, 0, 1)
+;
+
+-- pertaining to inventory table
+INSERT INTO inventory
+VALUES	(1234, 10567),
+		(1234, 10987),
+        (1235, 10999),
+        (1235, 11000),
+        (1236, 11111),
+        (1236, 11133),
+        (1237, 11233),
+        (1237, 10987)
+;
+
+-- pertaining to sales_contracts table
+INSERT INTO sales_contracts(customerName, customerEmail, vin, sale_date_and_time)
+VALUES	('Paul Tunda', 'pt@gmail.com', 11133, now()),		
+		('Mikel Arteta', 'ma@gmail.com', 11111, now());
+        
+UPDATE vehicles
+SET SOLD = 1
+WHERE vin = 11133;
+
+UPDATE vehicles
+SET SOLD = 1
+WHERE vin = 11111;
+
+-- pertaining to lease_contracts table
+INSERT INTO lease_contracts(customerName, customerEmail, vin, lease_date_and_time)
+VALUES	('Dana Wyatt', 'dana@texas.com', 10999, now()),		
+		('Zachary Westly', 'zach@texas.com', 10234, now());
+        
+UPDATE vehicles
+SET SOLD = 1
+WHERE vin = 10999;
+
+UPDATE vehicles
+SET SOLD = 1
+WHERE vin = 10234;
